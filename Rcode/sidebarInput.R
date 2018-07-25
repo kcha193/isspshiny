18e2966f8d166c90c3c1209e1d3319f97bb82dd7
Kevin Chang <kcha193@UoA.auckland.ac.nz>
2018-07-26 09:11:31 +1200

    minor back

--- Rcode/sidebarInput.R
+++ Rcode/sidebarInput.R
@@ -2,7 +2,7 @@
 
 
 # Module UI function
-sidebarInput <- function(id, date, link) {
+sidebarInput <- function(id, date) {
   # Create a namespace function using the provided id
   ns <- NS(id)
   
@@ -20,8 +20,6 @@ sidebarInput <- function(id, date, link) {
       selected = "None"
     ),
     box(
-      h4("Link to survey:"),
-      h5(a(link, href = link, target="_blank")),
       h4("Latest Update:"),
       h4(date),
       h4("Contact email:"),
